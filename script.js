34a7b34e596d1648e1a05c19c83a508e88bc7b72
momocode287 <moegiotsuka@gmail.com>
2026-07-16 10:25:33 +0900

    再々第一試作改訂

--- script.js
+++ script.js
@@ -40,9 +40,21 @@ document.getElementById("searchButton").addEventListener("click", function(){
 
 
 fetch("./train_data.csv")
-.then(response => response.text())
+    .then(response => {
+    console.log("HTTPステータス:", response.status);
+
+    if (!response.ok) {
+        throw new Error("HTTPエラー " + response.status);
+    }
+
+    return response.text();
+})
 .then(data => {
   
+    console.log("CSV読み込み成功!");
+    console.log(data);
+
+
     const rows = data.trim().split(/\r?\n/);
 
     console.log("rows:", rows.length);
@@ -54,8 +66,6 @@ fetch("./train_data.csv")
 const results = rows.filter(row => {
      const columns = row.split(",");
 
-     console.log("CSV:", columns[1], columns[3], colimns[4]);
-     console.log("選択:", station, line, direction);
 
      return (
         columns[1] === station &&
@@ -84,15 +94,12 @@ const best = results.filter(row => {
     return true;
 });
 
-console.log(best);
-
-console.log(best.length);
+console.log("best:", best.length);
 
 if (best.length > 0) {
    
     const columns = best[0].split(",");
-  
-    console.log(columns);
+
 
     const car = columns[7];
     const gate = columns[11];
@@ -101,29 +108,24 @@ if (best.length > 0) {
     const type = columns[14];
     const note = columns[15];
 
-    console.log(car);
-    console.log(gate);
-    console.log(exit);
-    console.log(transfer);
-    console.log(type);
-    console.log(note);
+
 
     document.getElementById("result").innerHTML = `
     
-    <h3>おすすめ号車</h3>
-    <p>${car}</p>
+        <h3>おすすめ号車</h3>
+        <p>${car}</p>
 
-   <h3>車両種別</h3>
-   <p>${type}</p>
+        <h3>車両種別</h3>
+        <p>${type}</p>
 
-   <h3>近い改札</h3>
-   <p>${gate}</p>
+        <h3>近い改札</h3>
+        <p>${gate}</p>
 
-    <h3>近い出口</h3>
-    <p>${exit}</p>
+        <h3>近い出口</h3>
+        <p>${exit}</p>
     
-    <h3>乗り換え先</h3>
-    <p>${transfer}</p>
+        <h3>乗り換え先</h3>
+        <p>${transfer}</p>
 
     ${note ? ` <h3>注意事項</h3><p>${note}</p>` : ""}
     
@@ -138,10 +140,11 @@ if (best.length > 0) {
 })
 .catch(error => {
     console.error(error);
+    alert(error.message);
 
     document.getElementById("result").innerHTML =
-    "<p>データの読み込みに失敗しました。</p><p>" + error.message + "</p>";
-    
+    "<p>データの読み込みに失敗しました。</p>";
+
 
 });
 
